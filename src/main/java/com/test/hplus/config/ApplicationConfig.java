22cb2f2398cd13a1162a6458d91fa2b988bb1320
Benny Chan <bennyx93@tamu.edu>
2022-02-13 15:38:43 -0600

    Implement chaining view resolvers

--- src/main/java/com/test/hplus/config/ApplicationConfig.java
+++ src/main/java/com/test/hplus/config/ApplicationConfig.java
@@ -4,6 +4,7 @@ import com.test.hplus.convertors.StringToEnumConvertor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -12,6 +13,8 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.ResourceBundleViewResolver;
+import org.springframework.web.servlet.view.XmlViewResolver;
 
 @Configuration
 @ComponentScan(basePackages = "com.test.hplus")
@@ -21,15 +24,6 @@ public class ApplicationConfig extends WebMvcConfigurationSupport {
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("css/**", "images/**")
                 .addResourceLocations("classpath:/static/css/", "classpath:/static/images/");
-    }
-
-    @Bean
-    public InternalResourceViewResolver jspViewResolver(){
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/jsp/");
-        viewResolver.setSuffix(".jsp");
-        viewResolver.setViewClass(JstlView.class);
-        return viewResolver;
     }
 
     @Override
@@ -49,4 +43,31 @@ public class ApplicationConfig extends WebMvcConfigurationSupport {
         executor.setThreadNamePrefix("hplusapp-thead-");
         return executor;
     }
+
+//    Application can configure multiple view resolvers
+
+    @Bean
+    public InternalResourceViewResolver jspViewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setOrder(2);
+        return viewResolver;
+    }
+
+    @Bean
+    public XmlViewResolver xmlViewResolver() {
+        XmlViewResolver viewResolver = new XmlViewResolver();
+        viewResolver.setLocation(new ClassPathResource("views.xml"));
+        viewResolver.setOrder(1);
+        return viewResolver;
+    }
+
+//    @Bean
+//    public ResourceBundleViewResolver resourceBundleViewResolver() {
+//        ResourceBundleViewResolver viewResolver = new ResourceBundleViewResolver();
+//        viewResolver.setBasename("views");
+//        return viewResolver;
+//    }
 }
