a4425e9f0e7c6d61c1a86413c5ea861add51096c
Benny Chan <bennyx93@tamu.edu>
2022-02-14 08:29:38 -0600

    Implement rest controllers for products and login

--- src/main/java/com/test/hplus/exceptions/ApplicationHandlerException.java
+++ src/main/java/com/test/hplus/exceptions/ApplicationHandlerException.java
@@ -1,5 +1,7 @@
 package com.test.hplus.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
@@ -11,5 +13,11 @@ public class ApplicationHandlerException {
     public String handleException() {
         System.out.println("in global exception handler");
         return "error";
+    }
+
+    @ExceptionHandler(LoginFailureException.class)
+    public ResponseEntity handleLoginFailure(LoginFailureException ex) {
+        System.out.println("in global exception handler");
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
     }
 }
